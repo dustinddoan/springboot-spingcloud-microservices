13f74935bb75e5b6f88998333097c7ffe471c145
dustinddoan <dustin.ddoan@gmail.com>
2023-09-04 21:01:52 -0700

    Spring Boot Security with Okta2

---
+++ api-gateway/src/main/java/net/alienzone/apigateway/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,27 @@
+package net.alienzone.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        http
+                .authorizeExchange(authorize -> authorize
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(withDefaults());
+
+        return http.build();
+
+    }
+}
